63554b66ab3f08c05a461e9df5e890acee6fd1a3
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2020-11-22 17:22:15 +0100

    Template, fixes

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from main.views import home
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='judge-home')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def home():
-    return HttpResponse("<h1>Works</h1>")
+def home(request):
+    return render(request, "base.html", {"title": "Home"})
