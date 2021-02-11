80bca4308c1e3ea1edba92d35a1f223afb76de6d
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2021-02-11 23:24:28 +0100

    Login, logout and register

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
-from main.views import home
+from main.views import home, register
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', home, name='judge-home')
+    path('', home, name='judge-home'),
+    path('login/', auth_views.LoginView.as_view(), name='judge-login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='judge-logout'),
+    path('register/', register, name='judge-register'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
 def home(request):
-    return render(request, "base.html", {"title": "Home"})
+    return render(request, "home.html", {"title": "Home"})
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("judge-home")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/register.html", {"form": form})
